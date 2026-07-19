64ab18a175a2f90c6fce9ef359db155f6c514062
xingyunyimo <huangjiawo@hotmail.com>
2026-07-19 13:35:55 +0800

    Bump version to v1.0.4

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.minirili.app"
         minSdk = 26
         targetSdk = 35
-        versionCode = 103
-        versionName = "1.0.3"
+        versionCode = 104
+        versionName = "1.0.4"
     }
 
     buildTypes {
